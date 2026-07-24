205fb8464efa3abd0d3386623465cb8f6490729a
silvaalex <silva_jralex@hotmail.com>
2026-07-23 21:19:17 -0300

    feat: add CI/CD workflow and implement custom application icon

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,28 @@ mod infrastructure;
 mod ui;
 
 use ui::SoundpadApp;
+use egui::IconData;
+
+fn load_icon() -> IconData {
+    let image = image::load_from_memory(include_bytes!("../assets/icon-1024.png"))
+        .unwrap()
+        .into_rgba8();
+
+    let (width, height) = image.dimensions();
+
+    IconData {
+        rgba: image.into_raw(),
+        width,
+        height,
+    }
+}
 
 fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions {
         viewport: eframe::egui::ViewportBuilder::default()
             .with_inner_size([700.0, 500.0])
-            .with_title("Soundpad / Bard Minstrel"),
+            .with_title("Soundpad / Bard Minstrel")
+            .with_icon(load_icon()),
         ..Default::default()
     };
 
